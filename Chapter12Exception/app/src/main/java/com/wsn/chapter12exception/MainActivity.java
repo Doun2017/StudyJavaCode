268ace8a7fff92d33095670e916152bbf1c3003a
DounWork <liushi2008wh@163.com>
2017-06-20 15:36:17 +0800

    practice12.1 完成

--- Chapter12Exception/app/src/main/java/com/wsn/chapter12exception/MainActivity.java
+++ Chapter12Exception/app/src/main/java/com/wsn/chapter12exception/MainActivity.java
@@ -2,12 +2,26 @@ package com.wsn.chapter12exception;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //practice12.1
+        try{
+            throw new Exception("message");
+
+        } catch (Exception e){
+            Log.d(TAG, e.getMessage());
+
+        }finally {
+            Log.d(TAG, "finally executed");
+        }
+
     }
 }
