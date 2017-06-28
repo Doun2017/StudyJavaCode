2fe4d8e728b298311ac554cdd8173f967e301650
Doun <liushi2008wh@163.com>
2017-06-29 00:07:51 +0800

    practice 13.6 完成

--- Chapter13String/app/src/main/java/com/wsn/chapter13string/MainActivity.java
+++ Chapter13String/app/src/main/java/com/wsn/chapter13string/MainActivity.java
@@ -31,5 +31,33 @@ public class MainActivity extends AppCompatActivity {
 
         //practice13.4 Receipt.java
 
+        //practice13.5 略
+
+        //practice13.6
+        MyFormatClass myFormatClass = new MyFormatClass();
+        Log.d(TAG, myFormatClass.toString());
+
+    }
+}
+
+class MyFormatClass{
+    private int anInt=2;
+    private long aLong=22;
+    private float aFloat=22.33f;
+    private double aDouble = 3333.133d;
+
+    @Override
+    public String toString() {
+//        return "MyFormatClass{" +
+//                "anInt=" + anInt +
+//                ", aLong=" + aLong +
+//                ", aFloat=" + aFloat +
+//                ", aDouble=" + aDouble +
+//                '}';
+        return String.format("MyFormatClass{" +
+                "anInt=%9d" +
+                ", aLong=%9d" +
+                ", aFloat=%9.5f" +
+                ", aDouble=%9.5f" +'}', anInt, aLong, aFloat, aDouble);
     }
 }
