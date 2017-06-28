27264a53514620f05443ff8b57b373694270fc48
DounWork <liushi2008wh@163.com>
2017-06-28 16:08:45 +0800

    practice13.1 完成

--- Chapter13String/app/src/main/java/com/wsn/chapter13string/MainActivity.java
+++ Chapter13String/app/src/main/java/com/wsn/chapter13string/MainActivity.java
@@ -4,10 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //practice13.1
+//        public String toString () {
+//            return "valve1 = " + valve1 + " " +
+//                    "valve2 = " + valve2 + " " +
+//                    "valve3 = " + valve3 + " " +
+//                    "valve4 = " + valve4 + "\n" +
+//                    "i = " + i + " " + "f = " + f + " " +
+//                    "source = " + source;
+//        }
+        //toString中没有使用循环，所以编译器默认只生成一个StringBuild对象。
+
+
+
+
     }
 }
