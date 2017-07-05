65330725c5fc017f447861e1189adc3c541280d7
Doun <liushi2008wh@163.com>
2017-07-05 23:49:39 +0800

    将practice14.3 完结

--- Chapter14RTTI/app/src/main/java/com/example/doun/chapter14rtti/MainActivity.java
+++ Chapter14RTTI/app/src/main/java/com/example/doun/chapter14rtti/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.doun.chapter14rtti;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     public final static String TAG = "MainActivity";
@@ -19,5 +20,15 @@ public class MainActivity extends AppCompatActivity {
         //practice14.2 见toys包
         //可正常显示新加的Interface
 
+        //practice14.3 见toys包
+        Rhomboid rhomboid = new Rhomboid();
+        Log.d(TAG, rhomboid.toString());
+        Shape shape = rhomboid;
+        Log.d(TAG, shape.toString());
+        Rhomboid rhomboid1 = (Rhomboid) shape;
+        Log.d(TAG, rhomboid1.toString());
+        //Triangle triangle = (Triangle)shape;// java.lang.ClassCastException:
+                                            // com.example.doun.chapter14rtti.Rhomboid cannot be cast to com.example.doun.chapter14rtti.Triangle
+        //Log.d(TAG, triangle.toString());
     }
 }
---
+++ Chapter14RTTI/app/src/main/java/com/example/doun/chapter14rtti/Shapes.java
@@ -0,0 +1,50 @@
+//: typeinfo/Shapes.java
+package com.example.doun.chapter14rtti;
+
+import java.util.*;
+
+abstract class Shape {
+    void draw() {
+        System.out.println(this + ".draw()");
+    }
+
+    abstract public String toString();
+}
+
+class Circle extends Shape {
+    public String toString() {
+        return "Circle";
+    }
+}
+
+class Square extends Shape {
+    public String toString() {
+        return "Square";
+    }
+}
+
+class Triangle extends Shape {
+    public String toString() {
+        return "Triangle";
+    }
+}
+
+class Rhomboid extends Shape {
+    public String toString() {
+        return "Rhomboid";
+    }
+}
+
+public class Shapes {
+    public static void main(String[] args) {
+        List<Shape> shapeList = Arrays.asList(
+                new Circle(), new Square(), new Triangle()
+        );
+        for (Shape shape : shapeList)
+            shape.draw();
+    }
+} /* Output:
+Circle.draw()
+Square.draw()
+Triangle.draw()
+*///:~
