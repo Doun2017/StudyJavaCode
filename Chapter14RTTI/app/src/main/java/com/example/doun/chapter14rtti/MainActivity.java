5e7df694972d9827991c3ee5735c6ac59105e96e
Doun <liushi2008wh@163.com>
2017-07-05 23:09:19 +0800

    将practice14.1 完结

--- Chapter14RTTI/app/src/main/java/com/example/doun/chapter14rtti/MainActivity.java
+++ Chapter14RTTI/app/src/main/java/com/example/doun/chapter14rtti/MainActivity.java
@@ -4,10 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        //practice14.1 见toys包
+        //用Class.newInstance()来创建的类，必须带有默认的构造器
+
+
     }
 }
