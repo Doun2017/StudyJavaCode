99a9a0d0a1b93a3fa56abdfbb5b68aca9bbf5f36
DounWork <liushi2008wh@163.com>
2017-07-06 09:07:20 +0800

    将practice14.4 完结

--- Chapter14RTTI/app/src/main/java/com/example/doun/chapter14rtti/MainActivity.java
+++ Chapter14RTTI/app/src/main/java/com/example/doun/chapter14rtti/MainActivity.java
@@ -20,15 +20,38 @@ public class MainActivity extends AppCompatActivity {
         //practice14.2 见toys包
         //可正常显示新加的Interface
 
-        //practice14.3 见toys包
+        //practice14.3
+//        Rhomboid rhomboid = new Rhomboid();
+//        Log.d(TAG, rhomboid.toString());
+//        Shape shape = rhomboid;
+//        Log.d(TAG, shape.toString());
+//        Rhomboid rhomboid1 = (Rhomboid) shape;
+//        Log.d(TAG, rhomboid1.toString());
+//        //Triangle triangle = (Triangle)shape;// java.lang.ClassCastException:
+//                                            // com.example.doun.chapter14rtti.Rhomboid cannot be cast to com.example.doun.chapter14rtti.Triangle
+//        //Log.d(TAG, triangle.toString());
+
+        //practice14.4
         Rhomboid rhomboid = new Rhomboid();
         Log.d(TAG, rhomboid.toString());
         Shape shape = rhomboid;
         Log.d(TAG, shape.toString());
-        Rhomboid rhomboid1 = (Rhomboid) shape;
-        Log.d(TAG, rhomboid1.toString());
-        //Triangle triangle = (Triangle)shape;// java.lang.ClassCastException:
-                                            // com.example.doun.chapter14rtti.Rhomboid cannot be cast to com.example.doun.chapter14rtti.Triangle
-        //Log.d(TAG, triangle.toString());
+        if (shape instanceof Rhomboid){
+            Rhomboid rhomboid1 = (Rhomboid) shape;
+            Log.d(TAG, rhomboid1.toString());
+        }
+        if (shape instanceof Triangle) {
+            Triangle triangle = (Triangle) shape;
+            Log.d(TAG, triangle.toString());
+        }
+
+
+
+
+
+
+
+
+
     }
 }
