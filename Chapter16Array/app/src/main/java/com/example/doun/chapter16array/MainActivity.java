a1ac8e28b58147f018c5bfc10f517710c0933159
Doun <liushi2008wh@163.com>
2017-07-28 15:18:08 +0800

    完结练习16.2

--- Chapter16Array/app/src/main/java/com/example/doun/chapter16array/MainActivity.java
+++ Chapter16Array/app/src/main/java/com/example/doun/chapter16array/MainActivity.java
@@ -14,17 +14,20 @@ public class MainActivity extends AppCompatActivity {
 
 
         //practice 16.1
-        BerylliumSphere[] d = {new BerylliumSphere(),
-                new BerylliumSphere(), new BerylliumSphere()
-        };
-        showBerylliumSphere(d);
-        //不能使用聚集初始化
-//        showBerylliumSphere({new BerylliumSphere(),
+//        BerylliumSphere[] d = {new BerylliumSphere(),
 //                new BerylliumSphere(), new BerylliumSphere()
-//        });
-        //结论：You must use aggregate initialization at the point of an array variable’s
-//        definition. With dynamic initialization you can create and initialize an array
-//        object anywhere
+//        };
+//        showBerylliumSphere(d);
+//        //不能使用聚集初始化
+//        //showBerylliumSphere({new BerylliumSphere(),
+//      //          new BerylliumSphere(), new BerylliumSphere()
+//      //  });
+//      //结论：You must use aggregate initialization at the point of an array variable’s
+//      //definition. With dynamic initialization you can create and initialize an array
+//      //object anywhere
+
+        //practice 16.2
+        showBerylliumSphere(createBerylliumSphere(12));
 
 
     }
@@ -33,5 +36,12 @@ public class MainActivity extends AppCompatActivity {
         for (BerylliumSphere b:berylliumSpheres){
             Log.d(TAG, b.toString());
         }
+    }
+    public BerylliumSphere[] createBerylliumSphere(int size){
+        BerylliumSphere berylliumSphere[] = new BerylliumSphere[size];
+        for (int i=0;i<size;i++){
+            berylliumSphere[i] = new BerylliumSphere();
+        }
+        return berylliumSphere;
     }
 }
