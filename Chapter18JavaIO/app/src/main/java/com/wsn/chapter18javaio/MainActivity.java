0eedb61a35d59cf0bf356814d3cfc629ecaa29dc
DounWork <liushi2008wh@163.com>
2017-08-10 09:16:01 +0800

    创建工程javaIO

---
+++ Chapter18JavaIO/app/src/main/java/com/wsn/chapter18javaio/MainActivity.java
@@ -0,0 +1,20 @@
+package com.wsn.chapter18javaio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //practice 18.1
+
+
+
+    }
+}
