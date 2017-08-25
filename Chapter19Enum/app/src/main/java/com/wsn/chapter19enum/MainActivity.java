20020fb1bd480885eed7f11d6cf674f2a6acf46b
DounWork <liushi2008wh@163.com>
2017-08-25 09:05:52 +0800

    完结练习19.7

--- Chapter19Enum/app/src/main/java/com/wsn/chapter19enum/MainActivity.java
+++ Chapter19Enum/app/src/main/java/com/wsn/chapter19enum/MainActivity.java
@@ -3,6 +3,8 @@ package com.wsn.chapter19enum;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.EnumSet;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -26,6 +28,11 @@ public class MainActivity extends AppCompatActivity {
 //        enum (like Course from TIJ4). Try this as an additional exercise with TIJ4’s
 //        enumerated/menu/Meal.java program. Pay special attention to overcoming
 //        the JDK’s constraints on the java.lang.Enum class.
+
+        //practice 19.7 EnumSet是一个虚类，它有两个实现类JumboEnumSet和RegularEnumSet，前者
+        //处理多与64个元素的情况，后者处理最多64各元素的情况。EnumSet的主入口是static方法noneOf(),
+        //其他的static方法都会调用此方法。
+
 
 
 
