deaa71e045478aa0234dbf65a6e738ef62e76e2f
DounWork <liushi2008wh@163.com>
2017-08-25 15:56:15 +0800

    完结练习19.8

---
+++ Chapter19Enum/app/src/main/java/com/wsn/chapter19enum/PostOffice.java
@@ -0,0 +1,207 @@
+//: enumerated/PostOffice.java
+// Modeling a post office.
+package com.wsn.chapter19enum;
+
+import java.util.*;
+
+//import net.mindview.util.*;
+
+//import static net.mindview.util.Print.*;
+
+class Mail {
+    // The NO's lower the probability of random selection:
+    //留局候领
+    enum GeneralDelivery {
+        YES, NO1, NO2, NO3, NO4, NO5
+    }
+
+    //可扫描性
+    enum Scannability {UNSCANNABLE, YES1, YES2, YES3, YES4}
+
+    //可读性
+    enum Readability {ILLEGIBLE, YES1, YES2, YES3, YES4}
+
+    //地址正确性
+    enum Address {INCORRECT, OK1, OK2, OK3, OK4, OK5, OK6}
+
+    //返回地址性
+    enum ReturnAddress {MISSING, OK1, OK2, OK3, OK4, OK5}
+
+    //转发性
+    enum Retransmission {UNABLE, OK1, OK2, OK3, OK4, OK5}
+
+    GeneralDelivery generalDelivery;
+    Scannability scannability;
+    Readability readability;
+    Address address;
+    ReturnAddress returnAddress;
+    Retransmission retransmission;
+    static long counter = 0;
+    long id = counter++;
+
+    public String toString() {
+        return "Mail " + id;
+    }
+
+    public String details() {
+        return toString() +
+                ", General Delivery: " + generalDelivery +
+                ", Address Scanability: " + scannability +
+                ", Address Readability: " + readability +
+                ", Address Address: " + address +
+                ", Return address: " + returnAddress +
+                ", Retransmission ability: " + retransmission;
+    }
+
+    // Generate test Mail:
+    public static Mail randomMail() {
+        Mail m = new Mail();
+        m.generalDelivery = Enums.random(GeneralDelivery.class);
+        m.scannability = Enums.random(Scannability.class);
+        m.readability = Enums.random(Readability.class);
+        m.address = Enums.random(Address.class);
+        m.returnAddress = Enums.random(ReturnAddress.class);
+        m.retransmission = Enums.random(Retransmission.class);
+        return m;
+    }
+
+    public static Iterable<Mail> generator(final int count) {
+        return new Iterable<Mail>() {
+            int n = count;
+
+            public Iterator<Mail> iterator() {
+                return new Iterator<Mail>() {
+                    public boolean hasNext() {
+                        return n-- > 0;
+                    }
+
+                    public Mail next() {
+                        return randomMail();
+                    }
+
+                    public void remove() { // Not implemented
+                        throw new UnsupportedOperationException();
+                    }
+                };
+            }
+        };
+    }
+}
+
+public class PostOffice {
+    enum MailHandler {
+        GENERAL_DELIVERY {
+            boolean handle(Mail m) {
+                switch (m.generalDelivery) {
+                    case YES:
+                        System.out.println("Using general delivery for " + m);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        },
+        MACHINE_SCAN {
+            boolean handle(Mail m) {
+                switch (m.scannability) {
+                    case UNSCANNABLE:
+                        return false;
+                    default:
+                        switch (m.address) {
+                            case INCORRECT:
+                                return false;
+                            default:
+                                System.out.println("Delivering " + m + " automatically");
+                                return true;
+                        }
+                }
+            }
+        },
+        VISUAL_INSPECTION {
+            boolean handle(Mail m) {
+                switch (m.readability) {
+                    case ILLEGIBLE:
+                        return false;
+                    default:
+                        switch (m.address) {
+                            case INCORRECT:
+                                return false;
+                            default:
+                                System.out.println("Delivering " + m + " normally");
+                                return true;
+                        }
+                }
+            }
+        },
+        RETURN_TO_SENDER {
+            boolean handle(Mail m) {
+                switch (m.returnAddress) {
+                    case MISSING:
+                        return false;
+                    default:
+                        System.out.println("Returning " + m + " to sender");
+                        return true;
+                }
+            }
+        },
+        RETRANSMISSION {
+            boolean handle(Mail m) {
+                switch (m.retransmission) {
+                    case UNABLE:
+                        return false;
+                    default:
+                        System.out.println("Retransmission " + m + " good");
+                        return true;
+                }
+            }
+        };
+
+        abstract boolean handle(Mail m);
+    }
+
+    static void handle(Mail m) {
+        for (MailHandler handler : MailHandler.values())
+            if (handler.handle(m))
+                return;
+        System.out.println(m + " is a dead letter");
+    }
+
+    public static void main(String[] args) {
+        for (Mail mail : Mail.generator(20)) {
+            System.out.println(mail.details());
+            handle(mail);
+            System.out.println("*****");
+        }
+    }
+} /* Output:
+Mail 0, General Delivery: NO2, Address Scanability: UNSCANNABLE, Address Readability: YES3, Address Address: OK1, Return address: OK1
+Delivering Mail 0 normally
+*****
+Mail 1, General Delivery: NO5, Address Scanability: YES3, Address Readability: ILLEGIBLE, Address Address: OK5, Return address: OK1
+Delivering Mail 1 automatically
+*****
+Mail 2, General Delivery: YES, Address Scanability: YES3, Address Readability: YES1, Address Address: OK1, Return address: OK5
+Using general delivery for Mail 2
+*****
+Mail 3, General Delivery: NO4, Address Scanability: YES3, Address Readability: YES1, Address Address: INCORRECT, Return address: OK4
+Returning Mail 3 to sender
+*****
+Mail 4, General Delivery: NO4, Address Scanability: UNSCANNABLE, Address Readability: YES1, Address Address: INCORRECT, Return address: OK2
+Returning Mail 4 to sender
+*****
+Mail 5, General Delivery: NO3, Address Scanability: YES1, Address Readability: ILLEGIBLE, Address Address: OK4, Return address: OK2
+Delivering Mail 5 automatically
+*****
+Mail 6, General Delivery: YES, Address Scanability: YES4, Address Readability: ILLEGIBLE, Address Address: OK4, Return address: OK4
+Using general delivery for Mail 6
+*****
+Mail 7, General Delivery: YES, Address Scanability: YES3, Address Readability: YES4, Address Address: OK2, Return address: MISSING
+Using general delivery for Mail 7
+*****
+Mail 8, General Delivery: NO3, Address Scanability: YES1, Address Readability: YES3, Address Address: INCORRECT, Return address: MISSING
+Mail 8 is a dead letter
+*****
+Mail 9, General Delivery: NO1, Address Scanability: UNSCANNABLE, Address Readability: YES2, Address Address: OK1, Return address: OK4
+Delivering Mail 9 normally
+*****
+*///:~
