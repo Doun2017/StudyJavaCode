1d81d75be2a53ae8c3250234fa7a51dac27cdd9a
Doun <liushi2008wh@163.com>
2017-08-26 15:21:09 +0800

    完结练习17.1

--- Chapter17Containers/app/src/main/java/com/example/doun/chapter17containers/Practises.java
+++ Chapter17Containers/app/src/main/java/com/example/doun/chapter17containers/Practises.java
@@ -2,6 +2,7 @@ package com.example.doun.chapter17containers;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,6 +19,17 @@ public class Practises {
         //practice 17.1
         List<String> list = new ArrayList<>(names(6));
         List<String> list1 = new LinkedList<>(names(6));
+        System.out.println("初始状态");
+        System.out.println(list);
+        System.out.println(list1);
+        Collections.sort(list);
+        Collections.sort(list1);
+        System.out.println("排序后");
+        System.out.println(list);
+        System.out.println(list1);
+        Collections.shuffle(list);
+        Collections.shuffle(list1);
+        System.out.println("打乱后");
         System.out.println(list);
         System.out.println(list1);
 
