aa7cfece00182772cfd9b49a5160c57a0c2282ca
Doun <liushi2008wh@163.com>
2017-09-03 12:03:55 +0800

    建立工程Chapter21Concurrency，开始对java中并发的练习。

---
+++ Chapter21Concurrency/app/src/main/java/com/example/doun/chapter21concurrency/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.doun.chapter21concurrency;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        //practice 21.1 Practises.java
+
+    }
+}
