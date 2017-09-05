d512709240ffdc7c0a9f2b1e8055b5dc224c18a8
DounWork <liushi2008wh@163.com>
2017-09-05 10:59:40 +0800

    完成练习21.20

--- Chapter21Concurrency/app/src/main/java/com/example/doun/chapter21concurrency/LiftOff.java
+++ Chapter21Concurrency/app/src/main/java/com/example/doun/chapter21concurrency/LiftOff.java
@@ -2,22 +2,49 @@
 // Demonstration of the Runnable interface.
 package com.example.doun.chapter21concurrency;
 
+import java.util.concurrent.TimeUnit;
+
 public class LiftOff implements Runnable {
-  protected int countDown = 10; // Default
-  private static int taskCount = 0;
-  private final int id = taskCount++;
-  public LiftOff() {}
-  public LiftOff(int countDown) {
-    this.countDown = countDown;
-  }
-  public String status() {
-    return "#" + id + "(" +
-      (countDown > 0 ? countDown : "Liftoff!") + "), ";
-  }
-  public void run() {
-    while(countDown-- > 0) {
-      System.out.print(status());
-      Thread.yield();
+    protected int countDown = 10; // Default
+    private static int taskCount = 0;
+    private final int id = taskCount++;
+
+    public LiftOff() {
     }
-  }
+
+    public LiftOff(int countDown) {
+        this.countDown = countDown;
+    }
+
+    public String status() {
+        return "#" + id + "(" + (countDown > 0 ? countDown : "Liftoff!") + "), ";
+    }
+
+    public void run() {
+
+        //官方答案
+//        while(countDown-- > 0) {
+//            if(Thread.currentThread().isInterrupted()) {
+//                System.out.println("Interrupted: #" + id);
+//                return;
+//            }
+//            System.out.println(status());
+//            Thread.yield();
+//        }
+
+        while (countDown-- > 0) {
+            System.out.print(status());
+            Thread.yield();
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+
+            } catch (InterruptedException e) {
+                System.out.println("sleep interrupted");
+                System.out.println("Stopping " + this);
+                return;
+            }
+        }
+
+
+    }
 } ///:~
